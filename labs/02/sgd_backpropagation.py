438adb33c7c1abe642a878198baf256bd285bc4b
backopat <patrik.backo1@gmail.com>
2025-02-28 09:07:08 +0100

    Add solution to sgd_bacpropagation.py

--- labs/02/sgd_backpropagation.py
+++ labs/02/sgd_backpropagation.py
@@ -14,9 +14,9 @@ from npfl138 import MNIST
 
 parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
-parser.add_argument("--batch_size", default=50, type=int, help="Batch size.")
-parser.add_argument("--epochs", default=10, type=int, help="Number of epochs.")
-parser.add_argument("--hidden_layer", default=100, type=int, help="Size of the hidden layer.")
+parser.add_argument("--batch_size", default=64, type=int, help="Batch size.")
+parser.add_argument("--epochs", default=2, type=int, help="Number of epochs.")
+parser.add_argument("--hidden_layer", default=20, type=int, help="Size of the hidden layer.")
 parser.add_argument("--learning_rate", default=0.1, type=float, help="Learning rate.")
 parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
@@ -35,24 +35,22 @@ class Model(torch.nn.Module):
         )
         self._b1 = torch.nn.Parameter(torch.zeros(args.hidden_layer))
 
-        # TODO: Create the rest of the parameters:
-        # - _W2, which is a parameter of size `[args.hidden_layer, MNIST.LABELS]`,
-        #   initialized to `torch.randn` value with standard deviation 0.1,
-        # - _b2, which is a parameter of size `[MNIST.LABELS]` initialized to zeros.
-        self._W2 = ...
-        self._b2 = ...
+        self._W2 = torch.nn.Parameter(
+            torch.randn(args.hidden_layer, MNIST.LABELS) * 0.1,
+            requires_grad=True,  # This is the default.
+        )
+        self._b2 = torch.nn.Parameter(torch.zeros(MNIST.LABELS))
 
     def forward(self, inputs: torch.Tensor) -> torch.Tensor:
-        # TODO: Define the computation of the network. Notably:
-        # - start by casting the input uint8 images to float32 using `.to(torch.float32)`,
-        # - then divide the tensor by 255 to normalize it to the `[0, 1]` range,
-        # - then reshape it to the shape `[inputs.shape[0], -1]`;
-        #   the -1 is a wildcard which is computed so that the number
-        #   of elements before and after the reshape is preserved,
-        # - then multiply it by `self._W1` and then add `self._b1`,
-        # - apply `torch.tanh`,
-        # - finally, multiply the result by `self._W2` and then add `self._b2`.
-        return ...
+        x = inputs.to(torch.float32)
+        x = x / 255
+        x = x.reshape((inputs.shape[0], -1))
+
+        x = x @ self._W1 + self._b1
+        x = torch.tanh(x)
+        x = x @ self._W2 + self._b2
+
+        return x
 
     def train_epoch(self, dataset: MNIST.Dataset) -> None:
         self.train()
@@ -63,47 +61,27 @@ class Model(torch.nn.Module):
             # Size of the batch is `self._args.batch_size`, except for the last, which
             # might be smaller.
 
-            # TODO: Start by moving the batch data to the device where the model is.
-            # This is needed, because the data is currently on CPU, but the model might
-            # be on a GPU. You can move the data using the `.to(device)` method, and you
-            # can obtain the device of the model using for example `self._W1.device`.
-            images = batch["images"].to(...)
-            labels = batch["labels"].to(...)
+            images = batch["images"].to(self._W1.device)
+            labels = batch["labels"].to(self._W1.device).to(torch.int64)
 
-            # TODO: Compute the predicted logits of the batch images by calling `self(...)`.
-            logits = ...
+            logits = self(images)
 
-            # TODO: Compute the probabilities of the batch images using `torch.softmax`.
-            probabilities = ...
+            probabilities = torch.softmax(logits, dim=1)
 
-            # TODO: Manually compute the loss:
-            # - For every batch example, the loss is the categorical crossentropy of the
-            #   predicted probabilities and the gold label. To compute the crossentropy, you can
-            #   - either use `torch.nn.functional.one_hot` to obtain one-hot encoded gold labels
-            #     and then compute the crossentropy from the definition, or
-            #   - you can start by obtaining directly the model probabilities of the gold labels
-            #     by suitably indexing the predicted probabilities by the gold labels.
-            #   Note that it might be necessary to convert the labels from bytes to `torch.int64`.
-            # - Finally, compute the average across the batch examples.
-            loss = ...
+            labels_onehot = torch.nn.functional.one_hot(labels, MNIST.LABELS)
+            loss = torch.mean(- torch.sum(labels_onehot * torch.log(probabilities), dim=1))
 
             # We create a list of all parameters. Note that a `torch.nn.Module` automatically
             # tracks owned parameters, so we could also use `list(self.parameters())`.
             parameters = [self._W1, self._b1, self._W2, self._b2]
 
-            # TODO: Compute the gradient of the loss with respect to parameters using
-            # the backpropagation algorithm, by
-            # - first resetting the gradients of all parameters to zero with `self.zero_grad()`,
-            # - then calling `loss.backward()`.
-            ...
+            self.zero_grad()
+            loss.backward()
 
             gradients = [parameter.grad for parameter in parameters]
             with torch.no_grad():
                 for parameter, gradient in zip(parameters, gradients):
-                    # TODO: Perform the SGD update with learning rate `self._args.learning_rate`
-                    # for the parameter and computed gradient. You can modify the
-                    # parameter value directly with `-=`, or you can use `parameter.sub_`.
-                    ...
+                    parameter.sub_(self._args.learning_rate * gradient)
 
     def evaluate(self, dataset: MNIST.Dataset) -> float:
         self.eval()
@@ -111,14 +89,13 @@ class Model(torch.nn.Module):
             # Compute the accuracy of the model prediction
             correct = 0
             for batch in dataset.batches(self._args.batch_size):
-                # TODO: Compute the logits of the batch images as in the training,
-                # and then convert them to Numpy with `.numpy(force=True)`.
-                logits = ...
+                images = batch["images"].to(self._W1.device)
+                labels = batch["labels"].to(self._W1.device).numpy(force=True).astype(np.int64)
 
-                # TODO: Evaluate how many batch examples were predicted
-                # correctly and increase `correct` variable accordingly, assuming
-                # the model predicts the class with the highest logit/probability.
-                correct += ...
+                logits = self(images).numpy(force=True)
+
+                predictions = np.argmax(logits, axis=1)
+                correct += np.sum(predictions == labels)
 
         return correct / len(dataset)
 
@@ -153,16 +130,13 @@ def main(args: argparse.Namespace) -> tuple[float, float]:
         model = model.to(device="xpu")
 
     for epoch in range(args.epochs):
-        # TODO: Run the `train_epoch` with `mnist.train` dataset
-        ...
+        model.train_epoch(mnist.train)
 
-        # TODO: Evaluate the dev data using `evaluate` on `mnist.dev` dataset
-        dev_accuracy = ...
+        dev_accuracy = model.evaluate(mnist.dev)
         print("Dev accuracy after epoch {} is {:.2f}".format(epoch + 1, 100 * dev_accuracy), flush=True)
         writer.add_scalar("dev/accuracy", 100 * dev_accuracy, epoch + 1)
 
-    # TODO: Evaluate the test data using `evaluate` on `mnist.test` dataset
-    test_accuracy = ...
+    test_accuracy = model.evaluate(mnist.test)
     print("Test accuracy after epoch {} is {:.2f}".format(epoch + 1, 100 * test_accuracy), flush=True)
     writer.add_scalar("test/accuracy", 100 * test_accuracy, epoch + 1)
 
